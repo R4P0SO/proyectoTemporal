ebe08f63c206114084b91a663f430b47a2c79f9e
R4P0SO <alexandreraposocasas@gmail.com>
2020-10-09 10:58:24 +0200

    proyecto inicial

---
+++ Sumador.java
@@ -0,0 +1,12 @@
+/**
+ * Sumador
+ */
+public class Sumador {
+
+    public static void main(String[] args) {
+        int num1=3;
+        int num2=2;
+        int suma=num1+num2;
+        System.out.println("la suma vale" +suma);
+    }
+}
